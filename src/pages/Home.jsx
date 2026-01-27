250f6e626bac0fdce36d37a3bd76204ae900df5b
varis125 <varisanfalihramadhan@mail.ugm.ac.id>
2026-01-27 14:32:41 +0700

    try again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <section className="container mx-auto px-6 py-20">
         <MapSection 
           title="Lokasi" 
-          description="Dusun Pundong 1 terletak di kelurahan Tirtoadi, Kecamatan Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta. Dusun ini terdari dari 2 RW dan 6 RT." 
+          description="Dusun Pundong 1 terletak di kelurahan Tirtoadi, Kecamatan Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta. Dusun ini terdari dari 3 RW dan 6 RT." 
         />
       </section>
     </div>
